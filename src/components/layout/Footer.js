7e5fed883d2544ab18cbf566e1cb3673261ec506
Luis Felipe <lfas_al@hotmail.com>
2022-02-24 10:14:27 -0300

    [fix]Home layout

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,13 +1,13 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 function Footer() {
-    return (
-      <footer className={styles.footer}>
-        <p className={styles.copy_right}>
-          <span>Agenda</span> &copy; 2021
-        </p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copy_right}>
+        <span>Agenda</span> &copy; 2021
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
